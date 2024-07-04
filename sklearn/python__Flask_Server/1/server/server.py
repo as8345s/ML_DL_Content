32dda7ea119d36fd56a532ac69662cb87d8e9521
as8345s <Alexander.Schechtel@alumni.fh-aachen.de>
2024-07-04 08:39:56 +0200

    Update Notebooks
    - Fixed the issue with PyCharm.
    - Python Flask server runs well.

--- sklearn/python__Flask_Server/1/server/server.py
+++ sklearn/python__Flask_Server/1/server/server.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
+import util
 
 app = Flask(__name__)
 
 
+# A route to a dir. Here: it's blank and we just print Hallo.
+# - Can be used to test basic functionality.
+# - After Start: localhost:port/my_function
 @app.route('/my_function')
 def my_function():
     """Test Function
@@ -11,6 +15,33 @@ def my_function():
     return "Hallo"
 
 
+@app.route('/make_prediction', methods=['POST'])
+def predict():
+    """Route to the site /make_prediction
+    Here we collect the data that is needed for the model to make predictions.
+    :return:
+    """
+    # Collect all data.
+    # - Data can be checked for errors and so on.
+    credit_score = float(request.form['credit_score'])
+    country = float(request.form['country'])
+    gender  =float(request.form['gender'])
+    age = float(request.form['age'])
+    tenure  = float(request.form['tenure'])
+    balance = float(request.form['balance'])
+    products_number = float(request.form['products_number'])
+    credit_card      = float(request.form['credit_card'])
+    active_member    = float(request.form['active_member'])
+    estimated_salary = float(request.form['estimated_salary'])
+
+    response = jsonify({
+        'churn_prediction': util.make_prediction([[credit_score, country, gender, age, tenure, \
+                                                 balance, products_number, credit_card, active_member, \
+                                                   estimated_salary]])
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 
 if __name__ == "__main__":
--- sklearn/python__Flask_Server/1/server/util.py
+++ sklearn/python__Flask_Server/1/server/util.py
@@ -6,13 +6,33 @@ __model = None
 
 
 def load_artifacts():
-    print("Lade Model...")
+    """Load artifacts and other resources.
+    Here we load the model.
+    :return: None.
+    """
+    print("Load Model...")
 
-    with open("./artefakte/churn_predict.pickle", 'rb') as f:
+    with open("./server/artefakte/churn_predict.pickle", 'rb') as f:
+         global __model
          __model = pickle.load(f)
-    print("Model geladen")
+    print("Model loaded")
+    return None
 
+
+def make_prediction(X):
+    """Predict outcome.
+    This function takes columns/features that were used for training.
+    Here we could perform various transformation before the model gets the data.
+    :return: int
+    """
+    # ...
+    load_artifacts()
+    return float(__model.predict(X)[0])
 
 
 if __name__ == '__main__':
-    load_artifacts()
+    # Load model.
+    #load_artifacts()
+    # Test the predict function
+    print("Make Prediction:")
+    print(make_prediction( [ [0.39141414, 0.5, 0., 1.0, 0, 0.5, 0., 0, 1., 0.5627087] ] ) )
